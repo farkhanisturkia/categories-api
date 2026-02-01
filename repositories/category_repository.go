5dac495b6370dcaaf693bb8036a3004ecf81f8a8
farkhanisturkia <farkhan22032000@gmail.com>
2026-02-02 00:48:58 +0700

    fixing category repo

--- repositories/category_repository.go
+++ repositories/category_repository.go
@@ -15,7 +15,7 @@ func NewCategoryRepository(db *sql.DB) *CategoryRepository {
 }
 
 func (repo *CategoryRepository) GetAll() ([]models.Category, error) {
-	query := "SELECT id, name, price, stock FROM categories"
+	query := "SELECT id, name, Description FROM categories"
 	rows, err := repo.db.Query(query)
 	if err != nil {
 		return nil, err
@@ -25,7 +25,7 @@ func (repo *CategoryRepository) GetAll() ([]models.Category, error) {
 	categories := make([]models.Category, 0)
 	for rows.Next() {
 		var p models.Category
-		err := rows.Scan(&p.ID, &p.Name, &p.Price, &p.Stock)
+		err := rows.Scan(&p.ID, &p.Name, &p.Description)
 		if err != nil {
 			return nil, err
 		}
@@ -36,17 +36,17 @@ func (repo *CategoryRepository) GetAll() ([]models.Category, error) {
 }
 
 func (repo *CategoryRepository) Create(category *models.Category) error {
-	query := "INSERT INTO categories (name, price, stock) VALUES ($1, $2, $3) RETURNING id"
-	err := repo.db.QueryRow(query, category.Name, category.Price, category.Stock).Scan(&category.ID)
+	query := "INSERT INTO categories (name, Description) VALUES ($1, $2) RETURNING id"
+	err := repo.db.QueryRow(query, category.Name, category.Description).Scan(&category.ID)
 	return err
 }
 
 // GetByID - ambil category by ID
 func (repo *CategoryRepository) GetByID(id int) (*models.Category, error) {
-	query := "SELECT id, name, price, stock FROM categories WHERE id = $1"
+	query := "SELECT id, name, Description FROM categories WHERE id = $1"
 
 	var p models.Category
-	err := repo.db.QueryRow(query, id).Scan(&p.ID, &p.Name, &p.Price, &p.Stock)
+	err := repo.db.QueryRow(query, id).Scan(&p.ID, &p.Name, &p.Description)
 	if err == sql.ErrNoRows {
 		return nil, errors.New("category tidak ditemukan")
 	}
@@ -58,8 +58,8 @@ func (repo *CategoryRepository) GetByID(id int) (*models.Category, error) {
 }
 
 func (repo *CategoryRepository) Update(category *models.Category) error {
-	query := "UPDATE categories SET name = $1, price = $2, stock = $3 WHERE id = $4"
-	result, err := repo.db.Exec(query, category.Name, category.Price, category.Stock, category.ID)
+	query := "UPDATE categories SET name = $1, Description = $2 WHERE id = $4"
+	result, err := repo.db.Exec(query, category.Name, category.Description, category.ID)
 	if err != nil {
 		return err
 	}
